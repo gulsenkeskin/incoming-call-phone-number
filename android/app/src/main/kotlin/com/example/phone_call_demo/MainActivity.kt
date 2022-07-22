8588a37b4b8c9adfcb8cb0f927dec2a9e7945723
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-07-23 01:38:12 +0300

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/example/phone_call_demo/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/phone_call_demo/MainActivity.kt
@@ -1,6 +1,53 @@
 package com.example.phone_call_demo
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.os.Build
+import android.provider.Telephony
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.BinaryMessenger
+import io.flutter.plugin.common.EventChannel
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
+import android.telephony.PhoneStateListener
+import android.telephony.TelephonyManager
+
+
+class MainActivity : FlutterActivity() {
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        val callReceiver = object : EventChannel.StreamHandler, BroadcastReceiver() {
+            var eventSink: EventChannel.EventSink? = null
+            override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
+                eventSink = events
+            }
+
+            override fun onCancel(arguments: Any?) {
+                eventSink = null
+            }
+
+            override fun onReceive(p0: Context?, p1: Intent?) {
+                val telephony: TelephonyManager =
+                    context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+                telephony.listen(object : PhoneStateListener() {
+                    override fun onCallStateChanged(state: Int, incomingNumber: String) {
+                        super.onCallStateChanged(state, incomingNumber)
+                        eventSink?.success(incomingNumber)
+
+                    }
+                }, PhoneStateListener.LISTEN_CALL_STATE)
+
+            }
+        }
+
+        registerReceiver(callReceiver, IntentFilter("android.intent.action.PHONE_STATE"))
+        EventChannel(flutterEngine.dartExecutor.binaryMessenger, "com.example.app/smsStream")
+            .setStreamHandler(callReceiver)
+
+    }
 }
